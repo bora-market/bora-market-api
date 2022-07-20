5f7a9394e098863febc79870b4f5677a687377e8
la1av1a <zldqkrktqk@gmail.com>
2022-07-20 12:36:17 +0900

    Style: Boolean -> boolean

--- src/main/java/boramarket/boramarketapi/domain/entity/user/UserRepository.java
+++ src/main/java/boramarket/boramarketapi/domain/entity/user/UserRepository.java
@@ -2,9 +2,7 @@ package boramarket.boramarketapi.domain.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    Boolean existsByUserId(String userId);
+    boolean existsByUserId(String userId);
 }
