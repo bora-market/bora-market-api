3db4b0acdd6e08278b127d0d995efe3e814391fd
la1av1a <zldqkrktqk@gmail.com>
2022-07-20 12:34:29 +0900

    Style: public -> private

--- src/main/java/boramarket/boramarketapi/web/user/UserController.java
+++ src/main/java/boramarket/boramarketapi/web/user/UserController.java
@@ -3,6 +3,7 @@ package boramarket.boramarketapi.web.user;
 import boramarket.boramarketapi.domain.service.UserService;
 import boramarket.boramarketapi.web.user.dto.UserRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +16,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signUp")
-    public ResponseEntity<Boolean> signUp(@RequestBody UserRequestDto requestDto){
+    private ResponseEntity<Boolean> signUp(@RequestBody UserRequestDto requestDto){
 
         if(userService.signUp(requestDto))
             return new ResponseEntity<>(true, HttpStatus.OK);
 
         return new ResponseEntity<>(false,HttpStatus.OK);
     }
+
 }
