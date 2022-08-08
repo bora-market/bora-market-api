ca9c6bbe20d8cae8b2872a835480373b14bb2667
la1av1a <zldqkrktqk@gmail..com>
2022-08-08 23:30:14 +0900

    feat : springSecurity 권한 부여

--- src/main/java/boramarket/boramarketapi/config/security/SecurityConfig.java
+++ src/main/java/boramarket/boramarketapi/config/security/SecurityConfig.java
@@ -1,16 +1,22 @@
 package boramarket.boramarketapi.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    private final UserDetailServiceImpl userDetailService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -30,6 +36,8 @@ public class SecurityConfig {
                 .maximumSessions(1)
                 .maxSessionsPreventsLogin(false)
                 .expiredUrl("/duplicated-login");
+
+        http.userDetailsService(userDetailService);
 
 
         return http.build();
